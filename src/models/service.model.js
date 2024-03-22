d4fb355fec735e8d013892865c5e68c0724b2e05
Sahinur Islam <infosahinur@gmail.com>
2024-03-22 15:09:35 +0600

    [SETUP]

---
+++ src/models/service.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// Define the service schema
+const serviceSchema = new Schema({
+  name: String,
+  price: Number
+});
+
+// Define the category schema
+const categorySchema = new Schema({
+  id: String,
+  name: String,
+  service: [serviceSchema]
+});
+
+// Define the main schema
+const mainSchema = new Schema({
+  id: String,
+  name: String,
+  description: [String],
+  Categories: [categorySchema]
+});
+
+// Create and export the mongoose model
+module.exports = mongoose.model("Service", mainSchema);
