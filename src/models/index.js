ee229caadf71f9e3a3bcdb036f6367eb5729ba36
Sahinur Islam <infosahinur@gmail.com>
2024-04-02 10:41:30 +0600

    [ADD] notification model

--- src/models/index.js
+++ src/models/index.js
@@ -2,3 +2,5 @@ module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
 module.exports.Service = require('./service.model')
 module.exports.Tasks = require('./tasks.model')
+module.exports.Referral = require('./referral.model')
+module.exports.Notification = require('./notification.model')
